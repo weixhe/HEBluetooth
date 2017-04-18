533b801e5aedb92d499881e196575fa14e715bcb
weixhe <workerwei@163.com>
2017-04-18 22:22:49 +0800

    添加工具类

---
+++ HEBluetooth/Bluetooth/HEBluetoothUtility.m
@@ -0,0 +1,13 @@
+//
+//  HEBluetoothUtility.m
+//  HEBluetooth
+//
+//  Created by weixhe on 2017/4/18.
+//  Copyright © 2017年 com.fumubang. All rights reserved.
+//
+
+#import "HEBluetoothUtility.h"
+
+@implementation HEBluetoothUtility
+
+@end
